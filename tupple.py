92e721ce2d9977c52cb6d947df2e4d3f1c121200
gpoddar2 <gpoddar2@gmail.com>
2020-08-12 00:40:46 +0530

    Update tupple.py

--- tupple.py
+++ tupple.py
@@ -2,3 +2,12 @@
 coordinates = [4,5]
 coordinates[1] = 10 
 print(coordinates[1])
+
+enter = (input("enter a letter"))
+win = enter[::-1]
+print(win)
+
+if enter == win:
+    print("Palindrom")
+else:
+    print("not palindrome")
